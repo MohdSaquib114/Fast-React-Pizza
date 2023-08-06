87eb7b28875093a6f3532fe1e10391df6e0e3f9c
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2023-08-06 22:46:03 +0530

    Update LinkButton path

--- src/ui/Linkbutton.jsx
+++ src/ui/Linkbutton.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 
-function LinkButton({ children, to }) {
+const  LinkButton = ({ children, to })=> {
   const navigate = useNavigate();
   const className = 'text-sm text-blue-500 hover:text-blue-600 hover:underline';
 
