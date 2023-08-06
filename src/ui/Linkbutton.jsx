a6a36bce340ee9f8ad9cc63cba3e79b60cf85a14
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2023-08-06 22:39:04 +0530

    Add address generator features

--- src/ui/Linkbutton.jsx
+++ src/ui/Linkbutton.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function Linkbutton({children, to}) {
-    const navigate = useNavigate()
-    if(to === '-1')    <button onClick={() => navigate(-1)}>{children}</button>
+function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-600 hover:underline';
+
+  if (to === '-1')
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
+
   return (
-    <Link to={to} className='text-sm text-blue-500 hover:text-blue-600 hover:underline'>
+    <Link to={to} className={className}>
       {children}
     </Link>
-  )
+  );
 }
+
+export default LinkButton;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily:{
+      sans:'Robot Mono, monospace',
+    },
     extend: {},
   },
   plugins: [],
